d411e11481399c5fcba9d52fe1531b20246797b3
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-08-17 15:04:38 +0530

    ui design changes

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,15 +1,32 @@
+import React from 'react'
+import Image from "next/image"
 
-"use client";
+const page = () => {
+  return (
+    <div>
+        <div className='flex justify-center mb-2'>
+            <h1 class="text-slate-900 text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-white">Coming Soon!
+                Stay tuned for our launch !
+            </h1>
+        </div>
+        <div className='flex justify-center'>
+            <p className="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400"> Exciting Changes Ahead! Our website is currently under construction, and we're working hard to bring you a brand new experience. Stay tuned for our upcoming launch and get ready for something amazing!  </p>
+        </div>
+        <div className='flex justify-center'>
+            
+            <Image
+                src="/assets/images/website-maintenance.svg"
+                width={400}
+                height={400}
+                className='image-responsive'
+                alt='image'
+            
+            />
+        </div>
+    </div>
+  )
+}
 
-import { useEffect, useState } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+export default page
 
-const Users = () => {
-    <>
-       <h1> All Users </h1>
-
-    </>
-};
-
-export default Users;
 
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -15,6 +15,12 @@ export function MainNav({
         href="/"
         className="text-sm font-medium transition-colors hover:text-primary"
       >
+        Home
+      </Link>
+      <Link
+        href="dashboard"
+        className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary"
+      >
         Dashboard
       </Link>
       <Link
