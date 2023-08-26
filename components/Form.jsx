80373af5f8fd40b2c5f25421d5224725c92559b9
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2023-08-26 23:09:40 +0530

    test changes

--- components/Form.jsx
+++ components/Form.jsx
@@ -37,7 +37,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
 
   <Card>
       <CardHeader>
-        <CardTitle>Add Customer</CardTitle>
+        <CardTitle>Add New Customer</CardTitle>
         <CardDescription>
           Add a new Customer details.
         </CardDescription>
